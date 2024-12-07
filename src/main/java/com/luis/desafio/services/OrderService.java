14a52810a9a6a15fcd2617fe796bd4cddb4365a7
Luis Fernando <luis.almeida.faria@gmail.com>
2024-12-06 23:18:37 -0300

    correções

--- src/main/java/com/luis/desafio/services/OrderService.java
+++ src/main/java/com/luis/desafio/services/OrderService.java
@@ -1,14 +1,24 @@
 package com.luis.desafio.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luis.desafio.entities.Order;
 
 @Service
 public class OrderService {
+    
+    @Autowired
+    private ShippingService shippingService;
+
     public double total(Order order) {
 
-        return order.getBasic() - order.getBasic() * (order.getDiscount() / 100);
+        double total = order.getBasic() - order.getBasic() * (order.getDiscount() / 100);
+
+        total += shippingService.shipment(order);
+
+        return total;
         
     }
+    
 }
--- src/main/java/com/luis/desafio/services/ShippingService.java
+++ src/main/java/com/luis/desafio/services/ShippingService.java
@@ -1,6 +1,5 @@
 package com.luis.desafio.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luis.desafio.entities.Order;
@@ -8,13 +7,10 @@ import com.luis.desafio.entities.Order;
 @Service
 public class ShippingService {
 
-    @Autowired
-    private OrderService orderService;
-
     public double shipment(Order order) {
-        if (orderService.total(order) < 100) {
+        if (order.getBasic() < 100) {
             return 20.0;
-        } else if (orderService.total(order) < 200) {
+        } else if (order.getBasic() < 200) {
             return 12.0;
         } else {
             return 0;
